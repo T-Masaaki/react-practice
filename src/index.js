bf974b4574d4b9b67f43f9c327f28bb969af4dbd
T-Masaaki <sr.skyrocket@gmail.com>
2018-03-19 17:26:00 +0900

    Routerの追加

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 
-import App from './components/App';
+import Root from './components/Root';
 import todoApp from './reducers'
 
 const store = createStore(todoApp)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
